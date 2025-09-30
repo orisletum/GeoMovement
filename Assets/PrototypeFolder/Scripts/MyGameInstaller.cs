63f721e71aef7ea9c319c87b1d9336d1be62a134
EgorKislovskiy <orisletum@gmail.com>
2025-09-30 21:45:18 +0700

    Template

---
+++ Assets/PrototypeFolder/Scripts/MyGameInstaller.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using Zenject;
+
+namespace GeoMovement
+{
+    public class MyGameInstaller : MonoInstaller
+    {
+      
+        public override void InstallBindings()
+        {
+
+           
+        }
+    }
+}
